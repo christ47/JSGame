f1a640e62878988b33f81369cf021f2a3f1a27c9
Imani Christie <ichristie@spartaglobal.com>
2018-10-18 14:04:01 +0100

    Bugs Sorted. LeaderBoard Incl.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(wordGen) { // var myString = " "
+
+  // name what u wanna store
   document.getElementById("answerArea").disabled = true;
   var word = {
     wordData: [{
@@ -44,9 +46,35 @@ document.addEventListener('DOMContentLoaded', function(wordGen) { // var myStrin
         name: "Orange",
         value: "brown"
       },
-      // {name:"", value=""};
-      // {name:"", value=""};
-      // {name:"", value=""};
+      {
+        name: "blue",
+        value: "blue"
+      },
+      //
+      {
+        name: "white",
+
+        value: "white"
+      },
+      {
+        name: "Pigs are...",
+        value: "pink"
+      },
+      //
+      {
+        name:"Paprika is ORANGE",
+       value:"white"
+     },
+     {
+       name: "apples are...",
+       value:"red"
+
+     },
+     {
+       name: "chalk is...",
+       value: "green"
+
+     }
 
     ]
   };
@@ -54,6 +82,10 @@ document.addEventListener('DOMContentLoaded', function(wordGen) { // var myStrin
   // document.getElementById('pointStatus').innerHTML = playerScore
   var correctAnswer = 0;
   var wordValue;
+  var topscoringplayer = JSON.parse(localStorage.getItem("Name"));
+  var highestScore = JSON.parse(localStorage.getItem("Scores"));
+  console.log(highestScore);
+    document.getElementById('topScore').innerHTML = "Name:  "+ topscoringplayer + "   Score:  "+ highestScore;
 
   function wordGen() {
     var randomNum = Math.floor(Math.random() * 9);
@@ -76,31 +108,48 @@ document.addEventListener('DOMContentLoaded', function(wordGen) { // var myStrin
   // CREATE
   gameStart.addEventListener('click', function(event) {
     wordGen();
+    playerScore = 0;
     var count = 9;
+    document.getElementById('scorePlace').innerHTML = playerScore;
     document.getElementById("answerArea").disabled = false;
+    document.getElementById('answerArea').value = "";
     var timer = setInterval(function() {
-        $("#counter").html(count--);
+      $("#counter").html(count--);
+      // $('#typeLine').addClass(correctAnswer);
+      // wordValue = numberPick.value;
 
-        document.getElementById('scorePlace').innerHTML = playerScore;
-        // $("#show").click(function(){
-        // $("#answerArea").show();
-        if (count == -1) {
-          clearInterval(timer);
-          document.getElementById('typeLine').innerHTML = "Game over. Please press the Start Button to retry";
-          playerScore = 0;
-          document.getElementById("answerArea").disabled = true;
+      // $("#show").click(function(){
+      // $("#answerArea").show();
+      if (count == -1) {
+        document.getElementById("startBtn").disabled = false;
+        // playerScore = 0;
+        clearInterval(timer);
+        document.getElementById('typeLine').innerHTML = "Game over!";
+        playerScore = parseInt(document.getElementById("scorePlace").innerHTML);
+        document.getElementById("answerArea").disabled = true;
+        console.log(playerScore);
+        $("#typeLine").removeClass();
+        if (playerScore>highestScore) {
+          highestScoreName = prompt('WOAH! You got the highest score! Whats your name?')
+          localStorage.setItem("Name", JSON.stringify(highestScoreName));
+          localStorage.setItem("Scores", JSON.stringify(playerScore));
 
-          $("#answerArea").keypress(function keyClick(event) {
-            if (event.keyCode === 13.) {
-              // $(this).hide();
-              // console.log(count);
-              document.getElementById('pointStatus').innerHTML = "Game over";
-            }
-            // alert("please restart");
-          });
-
+        //       $("#typeLine").removeClass();
         }
-      }, 1000);
+        // $("#answerArea").keypress(function keyClick(event) {
+        //   if (event.keyCode === 13.) {
+        //     // $(this).hide();
+        //     // console.log(count);
+        //     // document.getElementById('pointStatus').innerHTML = "Game over";
+        //   }
+        //   // alert("please restart");
+        // });
+
+      } else if (count > -1) {
+        document.getElementById("startBtn").disabled = true;
+
+      }
+    }, 1000);
   });
 
   // keydown addlistener
@@ -119,21 +168,17 @@ document.addEventListener('DOMContentLoaded', function(wordGen) { // var myStrin
     }
     return hash;
   }
-
   randomColorBg();
 
   function setBackground() {
     $(".inGame").css("background-color", randomColorBg());
   }
   setBackground();
-
   // $('#answerArea').keypress(keyClick);
   $("#answerArea").keypress(function keyClick(event) {
     if (event.keyCode === 13.) {
       // $("#typeLine").click();
       // alert("button clicks");
-
-
       var playersAnswer = document.getElementById('answerArea').value;
 
       console.log(playersAnswer);
@@ -181,243 +226,70 @@ document.addEventListener('DOMContentLoaded', function(wordGen) { // var myStrin
   });
 
   $("#easyMode").click(function() {
-    $("#typeLine").stop();
-    $('#typeLine').css('height:','4000px');
-    // $('.myElementClass').css('text-align','center');
+    location.reload();
   });
-});
-// })
+
+
+  $("#wackyMode").click(function() {
+
+    wackOut();
+
+    function wackOut() {
+      //       $('.button').click(function() {
+      $('#typeLine').animate({
+        deg: 18000
+      }, {
+        duration: 150000,
+        step: function(now) {
+          $(this).css({
+            transform: 'rotate(' + now + 'deg)',
+            wackOut
+          })
+
+        }
+      })
+
+    }
+  })
+  // reverseWackout();
+})
+// function reverseWackout() {
+//   $('#typeLine').animate({
 //
+//         deg: 1800
+//       },
+//       {
+//         duration: 15000,
+//         step: function(now) {
+//           $(this).css({
+//             transform: 'rotate(' + now + 'deg)'
+//             // wackOut();
+//           })
+//         }
+//       })
 //   }
+// }
+
+
+// });
 // })
+
+// // });
+
+// $("#img").rotate({bind:{
+//   click: function(){
+//     $(this).rotate({
+//       angle: 0,
+//       animateTo:180,
+//       callback: function(){   alert(1)  }
+//       })
+//     }
+//   }
+// });
+
+// time intervals first 15 secs x happens if time >30 at 15secs ...
+
 // ANIMATIONS
-// $(document).ready(function(){
-
-// }
-
-
-// function hardloop() {
-//     $('#typeLine').css({right:0});
-//     $('#typeLine').animate ({
-//         right: '+=1400',
-//     }, 3000, 'linear', function() {
-//         hardloop();
-//     });
-// }
-// hardloop();
-//   });
-//       $(document).ready(function() {
 //
-// })
-// $(document).ready(function() {
-//     function loop() {
-//         $('#clouds').css({right:0});
-//         $('#clouds').animate ({
-//             right: '+=1400',
-//         }, 5000, 'linear', function() {
-//             loop();
-//         });
-//     }
-//     loop();
-// });
-//
-// $(document).ready(function(){
-// reslide();
-// });
-// function reslide(){
-// $(‘.slide-right’).css({width:’0′}).animate({width:’100%’}, 400, reslide);
-// }
-
-
-//       // color of text
-//     // points +1
-//       // then count a point
-//       return document.getElementById('pointStatus').innerHTML = "Correct"
-//       // points +1
-//     }
-//     else {
-//       // retry return not quite
-//
-//       return document.getElementById('pointStatus').innerHTML = "Not quite! Try again"
-
-
-//     if (answerArea==) {
-
-//       // color of text
-//     // points +1
-//       // then count a point
-//       return document.getElementById('pointStatus').innerHTML = "Correct"
-//       // points +1
-//     }
-//     else {
-//       // retry return not quite
-//
-//       return document.getElementById('pointStatus').innerHTML = "Not quite! Try again"
-
-
-//
-// keydown center
-//
-
-// document.addEventListener('DOMContentLoaded',function(){
-//   document.getElementById("typeLine").addEventListener("ke", displayDate);
-//
-// function displayDate() {
-//     document.getElementById("demo").innerHTML = Date();
-//
-
-
-
-// });
-
-//     }
-//
-
-//
-//
-//
-
-
-
-// wordGen();
-
-// COUNTER
-// var count =  function setInterval(function(t) {
-//   for (var t = 90; t < 1; t--) {
-//     document.getElementById('timeSlot').innerHTML= t;
-//
-//     if (t<1) { clearInterval(count);
-//       document.getElementById('timeSlot').innerHTML = "Game over!";
-//     }
-//
-//     else {};
-//   }
-// }, 1000)
-//
-// count();
-
-
-
-
-
-
-
-// var deadline = new Date("Jan 5, 2018 15:37:25").getTime();
-// var deadline = new Date("Jan 5, 2018 15:37:25").getTime();
-// var x = setInterval(function() {
-// var now = new Date().getTime();
-// var t = deadline - now;
-// var days = Math.floor(t / (1000 * 60 * 60 * 24));
-// var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-// var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-// var seconds = Math.floor((t % (1000 * 60)) / 1000);
-// document.getElementById("demo").innerHTML = days + "d "
-// + hours + "h " + minutes + "m " + seconds + "s ";
-//     if (t < 0) {
-//         clearInterval(x);
-//         document.getElementById("demo").innerHTML = "EXPIRED";
-//     }
-// }, 1000);
-// //
-// // function startTimer(duration, display) {
-// //     var timer = duration, minutes, seconds;
-// //     setInterval(function () {
-// //         minutes = parseInt(timer / 60, 10)
-// //         seconds = parseInt(timer % 60, 10);
-// //
-// //         minutes = minutes < 10 ? "0" + minutes : minutes;
-// //         seconds = seconds < 10 ? "0" + seconds : seconds;
-// //
-// //         display.textContent = minutes + ":" + seconds;
-// //
-// //         if (--timer < 0) {
-// //             timer = duration;
-// //         }
-// //     }, 1000);
-// // }
-//
-// //
-// // <!-- Display the countdown timer in an element -->
-// // <p id="demo"></p>
-// //
-// // <script>
-// // // Set the date we're counting down to
-// // var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
-// //
-// // // Update the count down every 1 second
-// // var x = setInterval(function() {
-// //
-// //   // Get todays date and time
-// //   var now = new Date().getTime();
-// //
-// //   // Find the distance between now and the count down date
-// //   var distance = countDownDate - now;
-// //
-// //   // Time calculations for days, hours, minutes and seconds
-// //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-// //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-// //   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-// //
-// //   // Display the result in the element with id="demo"
-// //   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-// //   + minutes + "m " + seconds + "s ";
-// //
-// //   // If the count down is finished, write some text
-// //   if (distance < 0) {
-// //     clearInterval(x);
-// //     document.getElementById("demo").innerHTML = "EXPIRED";
-// //   }
-// // }, 1000);
-// // </script>
-// //
-// //
-// // var deadline = new Date("Jan 5, 2018 15:37:25").getTime();
-// // var deadline = new Date("Jan 5, 2018 15:37:25").getTime();
-// // var x = setInterval(function() {
-// // var now = new Date().getTime();
-// var t = deadline - now;
-// var days = Math.floor(t / (1000 * 60 * 60 * 24));
-// var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-// var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-// var seconds = Math.floor((t % (1000 * 60)) / 1000);
-// document.getElementById("demo").innerHTML = days + "d "
-// + hours + "h " + minutes + "m " + seconds + "s ";
-//     if (t < 0) {
-//         clearInterval(x);
-//         document.getElementById("demo").innerHTML = "GAMME OVER. Your Scored "+  " points";
-//     }
-// }, 1000);
-
-// Operation to make words changed
-// var count = 90;
-
-// function startCount() {
-
-// for (var i = 90; i >=0; i--) {
-//   setTimeout(function() {
-//     document.getElementById('counter').innerHTML = count;
-// } ,1000)}
-
-// function startCounter() {
-//   var count = 90, timer = setInterval(function() {
-//       $("#counter").html((count++)+1);
-//       if(count == 59) clearInterval(timer);
-//   }, 1000);
-// }
-
-// keyloging
-
-// var data  = {
-// lessons: [
-// {topic: "Business Skills", description:"Soft skills for making people not hate you."},
-// {topic: "HTML", description:"help to make lasagne."},
-// {topic: "CSS", description:"Concise Signage System."},
-// {topic: "JS", description:"Jumbo Shrimp."}
-// ]
-// }
-// var rand = Math.ceil(Math.random()*3);
-// console.log(data.lessons[rand].topic);
-// console.log(data.lessons[1].description);
-
 // location.reload();
+// $("#img").stopRotate();
